6248dede8fe0e0eb8de7653f317e71e8fbf5cbec
Roman Marchenko <whisperdepth@gmail.com>
2020-06-14 17:58:58 +0300

    v

--- lesson3/hw2/src/getBirthDate.js
+++ lesson3/hw2/src/getBirthDate.js
@@ -2,12 +2,9 @@ export default (dateString) => {
   const formatter = new Intl.DateTimeFormat('en', {
     year: '2-digit',
     month: 'short',
-    day: '2-digit',
   });
 
-  /* return `${new Date(dateString).getDate()} ${formatter.format(
+  return `${new Date(dateString).getDate()} ${formatter.format(
     new Date(dateString)
-  )}`; */
-
-  return formatter.format(new Date(dateString));
+  )}`;
 };
