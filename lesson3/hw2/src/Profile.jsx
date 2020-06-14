1327aae9e8dd0a8bea63b3fdd78fab19158039fc
Roman Marchenko <whisperdepth@gmail.com>
2020-06-14 15:41:57 +0300

    d

--- lesson3/hw2/src/Profile.jsx
+++ lesson3/hw2/src/Profile.jsx
@@ -3,14 +3,14 @@ import getBirthDate from './getBirthDate.js';
 
 const Profile = (props) => {
   return (
-    <>
+    <div>
       <div className='profile__name'>{`${props.user.firstName} ${props.user.lastName}`}</div>
       <div className='profile__birth'>{`Was born ${new Date(
         props.user.birthDate
       ).getDate()} ${getBirthDate(props.user.birthDate)} in ${
         props.user.birthPlace
       }`}</div>
-    </>
+    </div>
   );
 };
 
