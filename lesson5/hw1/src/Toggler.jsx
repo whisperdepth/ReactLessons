b9b0ae0a2716f62b90689baa97032cb059a1ba79
Roman Marchenko <whisperdepth@gmail.com>
2020-06-15 19:37:43 +0300

    f

--- lesson5/hw1/src/Toggler.jsx
+++ lesson5/hw1/src/Toggler.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
 
 class Toggler extends React.Component {
-  handleClick = (e) => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'Off',
+    };
+  }
+
+  /* handleClick = (e) => {
     e.target.textContent === 'Off'
       ? (e.target.textContent = 'On')
       : (e.target.textContent = 'Off');
+  }; */
+
+  handleClick = (e) => {
+    this.state.value === 'Off'
+      ? this.setState({
+          value: (this.state.value = 'On'),
+        })
+      : this.setState({
+          value: (this.state.value = 'Off'),
+        });
   };
 
   render() {
     return (
       <button className='toggler' onClick={this.handleClick}>
-        Off
+        {this.state.value}
       </button>
     );
   }
