3d98d8ed17518c9799f19d07ec5da19ecd0ad517
Roman Marchenko <whisperdepth@gmail.com>
2020-06-16 18:21:10 +0300

    c

--- lesson6/task2/src/index.jsx
+++ lesson6/task2/src/index.jsx
@@ -5,4 +5,4 @@ import Mailbox from './Mailbox.jsx';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<Mailbox unreadMessages={[]} />, rootElement);
+ReactDOM.render(<Mailbox unreadMessages={[1]} />, rootElement);
