576fb3c6f18ac48901cc9fa4fd8fcaef6e315520
Roman Marchenko <whisperdepth@gmail.com>
2020-06-18 20:40:40 +0300

    r

--- lesson7/hw1/src/TransactionsList.jsx
+++ lesson7/hw1/src/TransactionsList.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import Transaction from './Transaction';
 
 const TransactionsList = ({ transactions }) => {
-  const transactionsElems = transactions.map((transaction) => (
-    <Transaction key={transaction.id} {...transaction} />
-  ));
-  return <ul className='transactions'>{transactionsElems}</ul>;
+  return (
+    <ul className='transactions'>
+      {transactions.map((transaction) => (
+        <Transaction key={transaction.id} {...transaction} />
+      ))}
+    </ul>
+  );
 };
 
 export default TransactionsList;
