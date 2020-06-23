840cfb015fd89d3c350e609e36302ccef7781364
Roman Marchenko <whisperdepth@gmail.com>
2020-06-23 13:18:15 +0300

    g

---
+++ lesson9/task1/src/Search.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class Search extends React.Component {
+  state = {
+    value: '',
+  };
+
+  onChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
+
+  search = (e) => {
+    e.preventDefault();
+    console.log(this.state.value);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.search} className='search'>
+        <input
+          value={this.state.value}
+          onChange={this.onChange}
+          type='text'
+          className='search__input'
+        />
+        <button className='search__button'>Search</button>
+      </form>
+    );
+  }
+}
+
+export default Search;
