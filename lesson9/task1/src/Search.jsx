1ada4a3ebb3948417851e09964f92c133be93d10
Roman Marchenko <whisperdepth@gmail.com>
2020-06-23 13:22:53 +0300

    d

--- lesson9/task1/src/Search.jsx
+++ lesson9/task1/src/Search.jsx
@@ -13,7 +13,7 @@ class Search extends React.Component {
 
   search = (e) => {
     e.preventDefault();
-    alert(this.state.value);
+    alert(`Search text: ${this.state.value}`);
   };
 
   render() {
