824ce3ff4674e67955a3551ab03b81d9b094927a
Roman Marchenko <whisperdepth@gmail.com>
2020-06-24 14:11:02 +0300

    g

--- lesson9/hw1/src/Filter.jsx
+++ lesson9/hw1/src/Filter.jsx
@@ -5,7 +5,7 @@ const Filter = ({ filterText, count, onChange }) => {
    return (
         <div className="filter">
             <span className='filter__count'>{count}</span>
-            <input value={filterText} onChange={onChange} className='filter__text'/>
+            <input value={filterText} onChange={onChange} className='filter__input'/>
         </div>
     )
 }
