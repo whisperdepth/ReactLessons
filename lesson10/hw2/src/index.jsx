bdf40b44bd7fe47e0692b91b38f6c78f8c563f99
Roman Marchenko <whisperdepth@gmail.com>
2020-06-26 12:21:07 +0300

    a

---
+++ lesson10/hw2/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App.jsx';
+
+ReactDOM.render(<App userId='github' />, document.querySelector('#root'));
