193590d160e04dcb5358cd488c3239a9489b4012
Roman Marchenko <whisperdepth@gmail.com>
2020-07-16 12:02:24 +0300

    e

--- lesson14/hw2/src/App.jsx
+++ lesson14/hw2/src/App.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Clock from "./Clock.jsx";
 
 const App = () => {
+  const [visible, setVisibility] = useState(true);
+
   return (
     <div>
-      <Clock location={"New York"} offset={-5} />
-      <Clock location={"Kyiv"} offset={3} />
-      <Clock location={"London"} offset={0} />
+      <button onClick={() => setVisibility(!visible)}>Toggle</button>
+      <div>
+        {visible && (
+          <div>
+            <Clock location={"New York"} offset={-5} />
+            <Clock location={"Kyiv"} offset={3} />
+            <Clock location={"London"} offset={0} />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
